3c4cd9e53b5fe7021880dea045bd036520730503
sankaaaa <olexa2404@gmail.com>
2024-10-24 15:59:49 +0300

    done pest practice 4

---
+++ target/debug/build/thiserror-c2fc6fdbe7d2730d/build_script_build-c2fc6fdbe7d2730d.d
@@ -0,0 +1,5 @@
+C:\Users\olexa\rust-learning-path\pest_01\target\debug\build\thiserror-c2fc6fdbe7d2730d\build_script_build-c2fc6fdbe7d2730d.exe: C:\Users\olexa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.65\build.rs
+
+C:\Users\olexa\rust-learning-path\pest_01\target\debug\build\thiserror-c2fc6fdbe7d2730d\build_script_build-c2fc6fdbe7d2730d.d: C:\Users\olexa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.65\build.rs
+
+C:\Users\olexa\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.65\build.rs:
